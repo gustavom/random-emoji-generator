62327645c730bb52fbb19a8a170b41c27b789bd3
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2017-05-23 16:59:45 -0300

    first Commit

---
+++ app.js
@@ -0,0 +1,52 @@
+(function(win, doc){
+    'use strict';
+
+    var $formGenerate = doc.querySelector('[data-js="formGenerator"]'),
+        $emojiReceiver = doc.querySelector('[data-js="emoji-receiver"]');
+
+    var app = (function(){
+        return {
+            init: function(){
+                this.initEvents();
+            },
+            initEvents: function initEvents() {
+                $formGenerate.addEventListener('submit', this.handleSubmit, false);
+            },
+
+            handleSubmit: function handleSubmit(event){
+                event.preventDefault();
+                //console.log('evento de formulario')
+                app.emojiConection();
+            },
+
+            emojiConection: function emojiConection(){
+                var ajax = new XMLHttpRequest;
+                ajax.open('get', 'emojis.json', true);
+                ajax.send();
+                ajax.addEventListener('readystatechange', this.getEmoji, false);
+            },
+
+            getEmoji: function getEmoji(){
+                if (!app.isReady.call(this))
+                    return;
+                var data = JSON.parse(this.responseText);
+                
+                var emojiCount = Object.keys(data).length;
+                
+                $emojiReceiver.textContent = data[app.getRandomEmoji(emojiCount)];
+
+            },
+
+            getRandomEmoji: function getRandomEmoji(max){
+                return Math.floor(Math.random() * (max - 1)) + 1;
+            },
+
+            isReady: function isReady(){
+                return this.readyState === 4 && this.status === 200;
+            }
+        }
+    })();
+
+    app.init();
+
+})(window, document);
